9aaa79176ed75beb54383b6cf278d78b3f3fe44a
enesozmus <ensozmus@gmail.com>
2021-12-24 20:59:43 +0300

    Chapter17 | +

--- Chapter17/arrayList.cs
+++ Chapter17/arrayList.cs
@@ -23,14 +23,14 @@ namespace Chapter17
 
             // AddRange → bİr tür çoklu ekleme
             Console.WriteLine("***** AddRange *****");
-            string[] colors = { "yeşil", "siyah", "Akuamarin", "Gece Yarısı Mavisi"};
+            string[] colors = { "yeşil", "siyah", "Akuamarin", "Gece Yarısı Mavisi" };
             List<int> numbers = new List<int>() { 1, 3, 9, 7, 4, 5 };
             list.AddRange(colors);
             list.AddRange(numbers);
 
             foreach (var item in list)
                 Console.WriteLine(item);
-            
+
             // Sort → sıralama  → sorunlu tarafı → farklı veri türleri!
             Console.WriteLine("***** Sort *****");
             // list.Sort();
@@ -46,12 +46,54 @@ namespace Chapter17
             foreach (var item in list)
                 Console.WriteLine(item);
 
-            
+
             // Clear →
             Console.WriteLine("***** Clear *****");
-            // list.Clear(); 
+            // list.Clear();
+
+
+            Console.WriteLine("***** Dictionary *****");
+            Console.WriteLine("***** Dictionary *****");
+            // Dictionary
+            Dictionary<string, string> demo = new Dictionary<string, string>();
+            demo.Add("book", "kitap");
+            demo.Add("car", "araba");
+            demo.Add("pen", "kalem");
+            Dictionary<int, string> customers = new Dictionary<int, string>();
+            customers.Add(1, "enes");
+            customers.Add(2, "ali");
+            customers.Add(3, "ahmet");
+            customers.Add(4, "hasan");
+
+            Console.WriteLine(demo["book"]);  // kitap
+            Console.WriteLine(customers[3]);  // ahmet
+
+            // Keys and Values
+            Console.WriteLine("***** Keys and Values *****");
+            foreach (var demos in demo)
+            {
+                Console.WriteLine(demos);           // [book, kitap]    [car, araba]    [pen, kalem]
+                Console.WriteLine(demos.Key);       // book     car     pen
+                Console.WriteLine(demos.Value);     // kitap    araba   kalem
+            }
+
+            // ContainsKey
+            Console.WriteLine("***** ContainsKey *****");
+            Console.WriteLine(demo.ContainsKey("book"));      // True
+            Console.WriteLine(demo.ContainsKey("school"));    // False
+
+            // Count
+            Console.WriteLine("***** Count *****");
+            Console.WriteLine(demo.Count);              // 3
+            Console.WriteLine(customers.Count);         // 4
+
+            // Remove
+            Console.WriteLine("***** Remove *****");
+            customers.Remove(4);                        // [4, hasan]   kaldırıldı.
+            foreach (var customer in customers)
+                Console.WriteLine(customer);
         }
-    }   
+    }
     //  Farklı veri tipleri ArrayList içerisinde saklanabilir.
     //  Eleman eklenerek veya çıkarılarak ArrayList'in boyutu dinamik olarak değiştirilebilir.
     //  Varsayılan boyutu başlangıç olarak 4'tür. Aşıldığında otomatik olarak 2 katına çıkar.
